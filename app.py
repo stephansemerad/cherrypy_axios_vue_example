ee71e4313a7cdc17546e71be714d30db972d6fd8
Stephan <stephan.semerad@gmail.com>
2022-05-09 18:01:37 +0200

    completed lecture

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
 import cherrypy
 import os
 from mako.lookup import TemplateLookup
-lookup = TemplateLookup(
-    directories=['templates'], input_encoding='utf-8', output_encoding='utf-8', encoding_errors='replace'
-)
+import json
+
+path = os.path.join(os.getcwd(), 'templates')
+lookup = TemplateLookup(directories=[path])
 
 
-class webapp(object):
+class HelloWorld(object):
     @cherrypy.expose
-    def index(self):
+    def index(self, **kwargs):
+        print()
+        print('index')
+        print('--------')
+        print('kwargs:', kwargs)
+
         template = lookup.get_template("index.html")
+        return template.render(
+            id=kwargs.get('id', 1),
+            name=kwargs.get('name', None),
+            age=kwargs.get('age', None)
+        )
+
+    @cherrypy.expose
+    def get_users(self, **kwargs):
+
+        print()
+        print('get_users')
+        print('--------')
+        id = kwargs.get('id', 1)
+        print('id:', id)
+        print('kwargs:', kwargs)
+
+        if id == '1':
+            data = [{"name": "John", "age": 30},
+                    {"name": "James", "age": 29}]
+
+        if id == '2':
+            data = [{"name": "Leisheng", "age": 30},
+                    {"name": "Phillip", "age": 29}]
+
+        return json.dumps(data)
 
 
-cherrypy.quickstart(webapp, '/')
+cherrypy.quickstart(HelloWorld())
